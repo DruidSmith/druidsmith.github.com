b0ccae47ef11ff31707dd240efc4498d34e22ff9
Dave Smith <smith.davidg@epa.gov>
2026-09-14 11:13:03 -0400

    Corrected curly braces to get python to ignore instead of processing as f-string placeholders

--- build_thoughts.py
+++ build_thoughts.py
@@ -352,10 +352,10 @@ def wrap_with_layout(title, main_content_html, nav_rail_html, json_ld="", canoni
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4T0MRLF4V8"></script>
+    <script async src="https://googletagmanager.com"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag(){{dataLayer.push(arguments);}}
         gtag('js', new Date());
 
         gtag('config', 'G-4T0MRLF4V8');
